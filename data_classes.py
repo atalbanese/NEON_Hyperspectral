4478212692be6f082dfbc6fe837cdeebd7ddb93d
talbanese <tony.t.albanese@gmail.com>
2023-02-02 17:52:39 -0500

    got data classes working, ready to feed into model

--- data_classes.py
+++ data_classes.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
-import torch
 import itertools
 from typing import Literal
 import math
+from ortools.linear_solver import pywraplp
 
 class TreeData:
     def __init__(
@@ -18,6 +18,7 @@ class TreeData:
          taxa,
          plot_id, 
          site_id,
+         file_loc
         ):
         self.hyperspectral = hyperspectral
         self.rgb = rgb
@@ -26,29 +27,99 @@ class TreeData:
         self.hyperspectral_bands = hyperspectral_bands
         self.chm = chm
         self.utm_origin = utm_origin
-        self.taxa = taxa[()]
-        self.plot_id = plot_id[()]
-        self.site_id = site_id[()]
+        self.taxa = str(taxa[()])
+        self.plot_id = str(plot_id[()])
+        self.site_id = str(site_id[()])
+        self.file_loc = file_loc
 
     @classmethod
     def from_npz(cls, file_loc):
         with np.load(file_loc) as data:
-            new_instance = cls(**data)
+            new_instance = cls(**data, file_loc=file_loc)
         return new_instance
 
+    def apply_hs_filter(self, hs_filter):
+        # hs_filter should be a list of [min, max]
+        mask_list = [(self.hyperspectral_bands>=lmin) & (self.hyperspectral_bands<=lmax) for lmin, lmax in hs_filter]
+        band_mask = np.logical_or.reduce(mask_list)
+        idxs = np.where(band_mask)[0]
+        self.hyperspectral = self.hyperspectral[:,:,idxs]
+
+    def pad_arr(self, arr, out_dim):
+        three_d = len(arr.shape) == 3
+        y_pad = out_dim - arr.shape[0]
+        x_pad = out_dim - arr.shape[1]
+
+        if y_pad <0 or x_pad<0:
+            print(f"{self.file_loc}larger than given dimension, cropping..")
+            return arr[:out_dim, :out_dim,...]
+        else:
+            if three_d:
+                return np.pad(arr, [(0,y_pad), (0,x_pad), (0,0)])
+            else:
+                return np.pad(arr, [(0,y_pad), (0,x_pad)])
+
+
+    def get_dict(self, choices, out_dim, hs_filter = None):
+        return_dict = dict()
+
+        if hs_filter is not None:
+            self.apply_hs_filter(hs_filter)
+        
+        if "hs" in choices:
+            return_dict['hs'] = self.pad_arr(self.hyperspectral, out_dim)
+            return_dict['hs_mask'] = self.pad_arr(self.hyperspectral_mask, out_dim)
+        
+        if "chm" in choices:
+            return_dict['chm'] = self.chm, out_dim
+        
+        if "rgb" in choices:
+            return_dict["rgb"] = self.rgb
+        
+        if "origin" in choices:
+            return_dict["utm_origin"] = self.utm_origin
+
+
+        return return_dict
+
+
+
+
+class PlotData:
+    def __init__(
+        self,
+        name,
+        taxa,
+        init_tree = None
+    ):
+
+        self.name = name
+        self.trees = {t:[] for t in taxa}
+        if init_tree is not None:
+            self.trees[init_tree.taxa] = [init_tree]
+
+    def add_tree(self, tree: TreeData):
+        self.trees[tree.taxa].append(tree)
+
+
+    @property
+    def taxa_counts(self):
+        out = dict()
+        for k, v in self.trees.items():
+            out[k] = len(v)
+        return out
+
+    @property
+    def taxa_array(self):
+        return [v for v in self.taxa_counts.values()]
     
-    def get_masked_hs(self, out_type ='numpy'):
-        if out_type == 'numpy':
-            return self.hyperspectral[self.hyperspectral_mask]
-        if out_type == 'torch':
-            return torch.from_numpy(self.hyperspectral[self.hyperspectral_mask])
+    @property
+    def num_trees(self):
+        return sum([len(v) for v in self.trees.values()])
 
-
-    def get_masked_rgb(self, out_type ='numpy'):
-        if out_type == 'numpy':
-            return self.rgb[self.rgb_mask]
-        if out_type == 'torch':
-            return torch.from_numpy(self.rgb[self.rgb_mask])
+    @property
+    def all_trees(self):
+        return list(itertools.chain(*[v for v in self.trees.values()]))
 
 
 class SiteData:
@@ -58,13 +129,15 @@ class SiteData:
         random_seed: int,
         train: float,
         test: float,
-        valid: float
+        valid: float,
+        
         ):
 
         self.site_dir = site_dir
         self.all_trees = self.find_all_trees()
-        self.all_plots = self.find_all_plots()
         self.all_taxa = self.find_all_taxa()
+        self.all_plots = self.find_all_plots()
+        self.key = {k: ix for ix, k in enumerate(sorted(self.all_taxa.keys()))}
 
         self.rng = np.random.default_rng(random_seed)
         self.train_proportion = train
@@ -74,9 +147,24 @@ class SiteData:
         self.training_data = None
         self.testing_data = None
         self.validation_data = None
+        self.split_solution = None
 
 
+    @property
+    def taxa_counts(self):
+        out = dict()
+        for k, v in self.all_taxa.items():
+            out[k] = len(v)
+        return out
     
+    @property
+    def num_plots(self):
+        return len(self.all_plots.keys())
+
+    @property
+    def num_taxa(self):
+        return len(self.all_taxa.keys())
+
     def find_all_trees(self):
         all_dirs = [os.scandir(d) for d in os.scandir(self.site_dir) if d.is_dir()]
         return [TreeData.from_npz(f.path) for f in itertools.chain(*all_dirs) if f.name.endswith('.npz')]
@@ -85,9 +173,9 @@ class SiteData:
         plots_dict = dict()
         for tree in self.all_trees:
             if tree.plot_id in plots_dict:
-                plots_dict[tree.plot_id].append(tree)
+                plots_dict[tree.plot_id].add_tree(tree)
             else:
-                plots_dict[tree.plot_id] = [tree]
+                plots_dict[tree.plot_id] = PlotData(tree.plot_id, self.all_taxa.keys(), tree)
         return plots_dict
 
     def find_all_taxa(self):
@@ -125,8 +213,108 @@ class SiteData:
         self.testing_data = testing
         self.validation_data = validation
 
-    def make_plot_level_splits():
-        pass
+    def make_plot_level_splits(self):
+        training_goals = [math.floor(v*self.train_proportion) for v in self.taxa_counts.values()]
+        testing_goals = [math.floor(v*self.test_proportion) for v in self.taxa_counts.values()]
+        valid_goals = [v for v in self.taxa_counts.values()]
+        valid_goals = [v - training_goals[ix] -testing_goals[ix] for ix, v in enumerate(valid_goals)]
+
+        solutions = self.solve_splits([training_goals, testing_goals, valid_goals], ['training', 'testing', 'valid'])
+
+        #Get every tree from every selected plot and unpack them into a flat list
+        self.training_data = list(itertools.chain(*[self.all_plots[plot].all_trees for plot in solutions['training']]))
+        self.testing_data = list(itertools.chain(*[self.all_plots[plot].all_trees for plot in solutions['testing']]))
+        self.validation_data = list(itertools.chain(*[self.all_plots[plot].all_trees for plot in solutions['valid']]))
+        #Save the solution in case we need it later
+        self.split_solution = solutions
+
+    def solve_splits(self, goals_array, goals_labels, max_buffer=5, min_buffer=5) -> dict:
+  
+        taxa_counts = [plot.taxa_array for plot in self.all_plots.values()]
+        num_plots = len(self.all_plots.keys())
+        num_goals = len(goals_array)
+
+        solver = pywraplp.Solver.CreateSolver('SCIP')
+
+        vars_dict = dict()
+
+        #For each plot and each split category (testing, training, validation), we want to create a binary variable to choose which category that plot ends up in
+        for i, plot in enumerate(self.all_plots.keys()):
+            for j in range(num_goals):
+                vars_dict[i, j] = solver.IntVar(0, 1, plot)
+
+        #Our objective terms to minimize
+        objective_terms = []
+
+        #For each split category and each taxa, we want to find the difference between (taxa total across selected plots) and (category goal)
+        for i in range(num_goals):
+            for j in range(self.num_taxa):
+                #Taxa Goal for Category - Sum(Taxa count in assigned to category)
+                objective_terms.append(goals_array[i][j]- solver.Sum([vars_dict[k, i] * taxa_counts[k][j] for k in range(num_plots)]))
+
+                #Make sure things dont go too high or too low above the goal, theres no precise solution usually so we need to create a zone of constraints
+                #Theres definitely a way to do this by minimizing squared or absolute value difference instead but ortools is confusing
+                #Sum of taxa in a category must be less than the goal + buffer
+                solver.Add(solver.Sum([vars_dict[k, i] * taxa_counts[k][j] for k in range(num_plots)]) <= goals_array[i][j] + max_buffer)
+                #Sum of taxa in a category must be greater than the goal + buffer
+                solver.Add(solver.Sum([vars_dict[k, i] * taxa_counts[k][j] for k in range(num_plots)]) >= goals_array[i][j] - min_buffer)
+        
+        for i in range(num_plots):
+            solver.Add(solver.Sum([vars_dict[i, j] for j in range(num_goals)]) == 1)
+        
+        solver.Minimize(solver.Sum(objective_terms))
+        status = solver.Solve()
+
+        solutions_dict = {k:[] for k in goals_labels}
+        
+        #Split each plot into a category based on the solution.
+        for k, v in vars_dict.items():
+            solution_cat = goals_labels[k[1]]
+            if v.solution_value() == 1.0:
+                solutions_dict[solution_cat].append(v.name())
+
+        #Print the solution
+        sol_text = ""
+        for i, (k, v) in enumerate(solutions_dict.items()):
+            sol_text = sol_text + f"{k} Solution:\n"
+            for j, taxa in enumerate(self.all_taxa.keys()):
+                sol_text = sol_text + f'{taxa}: Goal: {goals_array[i][j]}, Solution: {sum([self.all_plots[plot].taxa_counts[taxa] for plot in v])}\n'
+        print(sol_text)
+        solutions_dict['sol_text'] = sol_text
+
+        return solutions_dict
+
+    def get_data(self, 
+        data_selection: Literal["training", "testing", "validation", "all"], 
+        data_choices, 
+        out_dim, 
+        hs_filter=[[410,1357],[1400,1800],[1965,2490]], 
+        make_key=False):
+        working_data = self.select_working_data(data_selection)
+
+        for tree in working_data:
+            to_return = tree.get_dict(data_choices, out_dim, hs_filter)
+            if make_key:
+                to_return['key'] = self.make_key(tree, out_dim)
+        
+            yield to_return
+    
+    def make_key(self, tree, out_dim):
+        new_key = np.zeros((out_dim, out_dim, self.num_taxa), np.float32)
+
+        this_tree = tree.taxa
+        new_key[...,self.key[this_tree]] = 1.0
+        return new_key
+
+    def select_working_data(self, data_selection):
+        if data_selection == "training":
+            return self.training_data
+        if data_selection == "testing":
+            return self.testing_data
+        if data_selection == "validation":
+            return self.validation_data
+        if data_selection == "all":
+            return self.all_trees
 
 
 if __name__ == "__main__":
@@ -134,7 +322,13 @@ if __name__ == "__main__":
         site_dir = r'C:\Users\tonyt\Documents\Research\thesis_final\NIWO',
         random_seed=42,
         train = 0.6,
-        test= 0.2,
-        valid = 0.2)
+        test= 0.1,
+        valid = 0.3)
 
-    test.make_splits('tree_level')
+    #test.all_trees[0].apply_hs_filter([[410,1357],[1400,1800],[1965,2490]])
+
+    test.make_splits('plot_level')
+    for x in test.get_data('training', ['hs', 'chm', 'rgb', 'origin'], 16, make_key=True):
+        print(x)
+
+   
